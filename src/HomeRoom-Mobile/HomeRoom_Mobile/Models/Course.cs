6fad180c95377a2af10a8632b37f1b6ad5637fa8
Chad Smith <Smith.Chad12@gmail.com>
2016-08-29 01:04:44 -0500

    Courses Test Page
    Testing what the courses page could look like

---
+++ src/HomeRoom-Mobile/HomeRoom_Mobile/Models/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeRoom_Mobile.Models
+{
+    public class Course
+    {
+        public string Name { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Teacher { get; set; }
+    }
+}
