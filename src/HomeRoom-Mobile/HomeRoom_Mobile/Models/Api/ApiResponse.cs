0da0ead2ec50c00bd39ce4a1c3bd0a5cc0f6b6d2
Chad Smith <Smith.Chad12@gmail.com>
2016-09-04 15:46:41 -0500

    Base api service
    Added a base api service for all requests to use and api response objects

---
+++ src/HomeRoom-Mobile/HomeRoom_Mobile/Models/Api/ApiResponse.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+
+namespace HomeRoom_Mobile.Models.Api
+{
+    /// <summary>
+    /// The base response object that nearly all Api requests will respond back with.
+    /// Holds all the basic information about a request
+    /// </summary>
+    /// <typeparam name="TJsonObject">The type of the json object that you expect Result to be</typeparam>
+    public class ApiResponse<TJsonObject>
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="ApiResponse{TJsonObject}"/> is successful.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if successful; otherwise, <c>false</c>.
+        /// </value>
+        [JsonProperty("success")]
+        public bool Success { get; set; }
+
+        /// <summary>
+        /// Gets or sets the expected api result.
+        /// </summary>
+        /// <value>
+        /// The data you expected back in a response.
+        /// </value>
+        [JsonProperty("result")]
+        public TJsonObject Result { get; set; }
+
+        /// <summary>
+        /// Gets or sets the error information from the request.
+        /// </summary>
+        /// <value>
+        /// The error.
+        /// </value>
+        [JsonProperty("error")]
+        public ApiError Error { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether [unauthorized result].
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if [unauthorized result]; otherwise, <c>false</c>.
+        /// </value>
+        [JsonProperty("unAuthorizedRequest")]
+        public bool UnauthorizedResult { get; set; }
+    }
+
+    /// <summary>
+    /// Basic error information about an api request.
+    /// </summary>
+    public struct ApiError
+    {
+        /// <summary>
+        /// Gets or sets the error code.
+        /// </summary>
+        /// <value>
+        /// The code.
+        /// </value>
+        [JsonProperty("code")]
+        public int Code { get; set; }
+
+        /// <summary>
+        /// Gets or sets the error message.
+        /// </summary>
+        /// <value>
+        /// The message.
+        /// </value>
+        [JsonProperty("message")]
+        public string Message { get; set; }
+    }
+}
---
+++ src/HomeRoom-Mobile/HomeRoom_Mobile/Services/BaseHttpService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using ModernHttpClient;
+using Newtonsoft.Json;
+
+namespace HomeRoom_Mobile.Services
+{
+    /// <summary>
+    /// Base Http Service to help send requests to an api.
+    /// </summary>
+    public abstract class BaseHttpService
+    {
+        /// <summary>
+        /// Sends the request asynchronously.
+        /// </summary>
+        /// <typeparam name="T">Type of the expected result from the server. For most requests this should be ApiResponse.</typeparam>
+        /// <param name="url">The URL.</param>
+        /// <param name="httpMethod">The HTTP method.</param>
+        /// <param name="headers">The headers.</param>
+        /// <param name="requestData">The request data.</param>
+        /// <returns></returns>
+        protected async Task<T> SendRequestAsync<T>(Uri url, HttpMethod httpMethod = null, IDictionary<string, string> headers = null, object requestData = null)
+        {
+            T result;
+
+            // default to GET if they didn't pass in a http method
+            var method = httpMethod ?? HttpMethod.Get;
+            // set the data to send if it is not null
+            var data = requestData == null ? null : JsonConvert.SerializeObject(requestData);
+
+
+            using (var request = new HttpRequestMessage(method, url))
+            {
+                // add the request content to the request
+                if (data != null)
+                {
+                    request.Content = new StringContent(data, Encoding.UTF8, "application/json");
+                }
+                // add any headers we need
+                if (headers != null)
+                {
+                    foreach (var header in headers)
+                    {
+                        request.Headers.Add(header.Key, header.Value);
+                    }
+                }
+                // create a client to send the request out
+                // this is ModernHttpClient instead of just the normal HttpClient, so we use a native message handler
+                // On Android this will use OkHttp, and on iOS will use NSURLSession
+                using (var client = new HttpClient(new NativeMessageHandler()))
+                {
+                    using (var response = await client.SendAsync(request, HttpCompletionOption.ResponseContentRead))
+                    {
+                        var content = response.Content == null ? null : await response.Content.ReadAsStringAsync();
+                        result = JsonConvert.DeserializeObject<T>(content);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
